2bcae9b8c0f86f03bfbc4c378c611d5aad33eea9
babytutu <babytutu@live.com>
2023-06-20 07:04:16 +0000

    Deploying to gh-pages from @ babytutu/react-app@151d10bc11433731fc4b39b8fad1e246f0667a01 🚀

--- _next/static/chunks/app/link/page-b5d36b6dd4765c24.js
+++
@@ -1,9 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[830],{3969:function(e,r,t){Promise.resolve().then(t.bind(t,5433))},5433:function(e,r,t){"use strict";t.r(r),t.d(r,{default:function(){return s}});var n=t(9268),o=t(6008);function s(){let e=(0,o.useSearchParams)(),r=e.get("link")||"";return(0,n.jsx)("iframe",{className:"iframe-warp",src:r})}},3177:function(e,r,t){"use strict";/**
- * @license React
- * react-jsx-runtime.production.min.js
- *
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */var n=t(6006),o=Symbol.for("react.element"),s=Symbol.for("react.fragment"),f=Object.prototype.hasOwnProperty,u=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,c={key:!0,ref:!0,__self:!0,__source:!0};function i(e,r,t){var n,s={},i=null,a=null;for(n in void 0!==t&&(i=""+t),void 0!==r.key&&(i=""+r.key),void 0!==r.ref&&(a=r.ref),r)f.call(r,n)&&!c.hasOwnProperty(n)&&(s[n]=r[n]);if(e&&e.defaultProps)for(n in r=e.defaultProps)void 0===s[n]&&(s[n]=r[n]);return{$$typeof:o,type:e,key:i,ref:a,props:s,_owner:u.current}}r.Fragment=s,r.jsx=i,r.jsxs=i},9268:function(e,r,t){"use strict";e.exports=t(3177)},6008:function(e,r,t){e.exports=t(3027)}},function(e){e.O(0,[253,488,744],function(){return e(e.s=3969)}),_N_E=e.O()}]);
